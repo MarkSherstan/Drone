eb3baf6dab7ec31181c5b12df2e146c49f8a0295
Mark Sherstan <mshersta@ualberta.ca>
2021-01-23 23:37:27 -0700

    Finish IMU start up checks

--- Firmware/lib/IMU/IMU.cpp
+++ Firmware/lib/IMU/IMU.cpp
@@ -1,11 +1,27 @@
 #include "IMU.h"
 #include <arduino.h>
 #include <Wire.h>
+#include "FlightControl.h"
 
 /// @brief Set the IMU address and check for positive connection.
 /// @param addr Hexadecimal address based on AD0 pin - 0x68 low or 0x69 high.
 IMU::IMU(unsigned char addr) {
+  // Set address
   _addr = addr;
+
+  // Find who the IMU is
+  Wire.beginTransmission(_addr);
+  Wire.write(WHO_AM_I);
+  Wire.endTransmission();
+  Wire.requestFrom(_addr, 1);
+
+  // Either show green and reset the IMU or show red
+  if (Wire.read() == 0x98){
+    write2bytes(PWR_MGMT_1, 0x00);
+    FC.statusLight('G');
+  } else {
+    FC.statusLight('R');
+  }
 }
 
 /// @brief Set the accelerometer full scale range.
@@ -14,18 +30,22 @@ IMU::IMU(unsigned char addr) {
 float IMU::getAres(int Ascale) {
   switch (Ascale){
     case AFS_2G:
+      // 2g
       _aRes = 16384.0;
       write2bytes(ACCEL_CONFIG, 0x00);
       return _aRes;
     case AFS_4G:
+      // 4g
       _aRes = 8192.0;
       write2bytes(ACCEL_CONFIG, 0x08);
       return _aRes;
     case AFS_8G:
+      // 8g
       _aRes = 4096.0;
       write2bytes(ACCEL_CONFIG, 0x10);
       return _aRes;
     case AFS_16G:
+      // 16g
       _aRes = 2048.0;
       write2bytes(ACCEL_CONFIG, 0x18);
       return _aRes;
@@ -38,21 +58,24 @@ float IMU::getAres(int Ascale) {
 /// @param Gscale Set 0 for ±250°/s, 1 for ±500°/s, 2 for ±1000°/s, and 3 for ±2000°/s.
 /// @return Sensitivity scale factor in LSB/(°/s).
 float IMU::getGres(int Gscale) {
-  // Set the full scale range for the gyroscope
   switch (Gscale){
     case GFS_250DPS:
+      // 250 deg/s
       _gRes = 131.0;
       write2bytes(GYRO_CONFIG, 0x00);
       return _gRes;
     case GFS_500DPS:
+      // 500 deg/s
       _gRes = 65.5;
       write2bytes(GYRO_CONFIG, 0x08);
       return _gRes;
     case GFS_1000DPS:
+      // 1000 deg/s
       _gRes = 32.8;
       write2bytes(GYRO_CONFIG, 0x10);
       return _gRes;
     case GFS_2000DPS:
+      // 2000 deg/s
       _gRes = 16.4;
       write2bytes(GYRO_CONFIG, 0x18);
       return _gRes;
