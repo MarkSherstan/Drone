9085e0b8eef8d983c6167959a8f57bf8cc22ce55
Mark Sherstan <mshersta@ualberta.ca>
2021-01-30 21:12:35 -0700

    Add PID initialization to main

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -2,10 +2,17 @@
 #include <Wire.h>
 #include "FlightControl.h"
 #include "IMU.h"
+#include "PID.h"
+
+// Set PID values
+gains_t rollGains {.P=1.0, .I=2.0, .D=3.0};
+gains_t pitchGains{.P=1.0, .I=2.0, .D=3.0};
+gains_t yawGains  {.P=1.0, .I=2.0, .D=3.0};
 
 // Connect to the IMU and configure the full scale range
 IMU imu(AD0_LOW, AFS_4G, GFS_500DPS);
 FlightControl FC;
+PID pid(rollGains, pitchGains, yawGains);
 
 void setup() {
   // Start up I2C
