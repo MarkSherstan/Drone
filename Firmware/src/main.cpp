5e1ab939aa0d77ad18c55fc0ac02e551689310c8
Mark Sherstan <mshersta@ualberta.ca>
2022-02-21 15:19:29 -0700

    Reflect receiver updates

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -10,11 +10,11 @@ Gains pGains{.P = 1.0, .I = 2.0, .D = 3.0};
 Gains yGains{.P = 1.0, .I = 2.0, .D = 3.0};
 
 // Set receiver calibration values
-ChannelCal ch1cal{.low = 1000, .high = 2000, .center = 1500, .reverse = 0};
-ChannelCal ch2cal{.low = 1000, .high = 2000, .center = 1500, .reverse = 0};
-ChannelCal ch3cal{.low = 1000, .high = 2000, .center = 1500, .reverse = 0};
-ChannelCal ch4cal{.low = 1000, .high = 2000, .center = 1500, .reverse = 0};
-ChannelCal ch5cal{.low = 1000, .high = 2000, .center = 1500, .reverse = 0};
+ChannelCal ch1cal{.low = 1000, .high = 2000, .center = 1500, .reverse = false};
+ChannelCal ch2cal{.low = 1000, .high = 2000, .center = 1500, .reverse = false};
+ChannelCal ch3cal{.low = 1000, .high = 2000, .center = 1500, .reverse = false};
+ChannelCal ch4cal{.low = 1000, .high = 2000, .center = 1500, .reverse = false};
+ChannelCal ch5cal{.low = 1000, .high = 2000, .center = 1500, .reverse = false};
 
 // IMU connection, general flight control, and receiver classes
 IMU imu(AD0_LOW, AFS_4G, GFS_500DPS);
@@ -62,7 +62,7 @@ void loop()
     imu.calcAttitude();
 
     // Update the receiver inputs
-    // FC.receiver();
+    RX.update();
 
     // Run PID controller
     rPID.update(imu.attitude.r);
