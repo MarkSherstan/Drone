d4d72b22e8b4e1e8cfc517eee793142c571c8d3b
Mark Sherstan <mshersta@ualberta.ca>
2022-02-21 07:44:37 -0700

    Formatting

--- Firmware/lib/FlightControl/FlightControl.cpp
+++ Firmware/lib/FlightControl/FlightControl.cpp
@@ -2,28 +2,33 @@
 #include <arduino.h>
 
 /// @brief Set digital pins as outputs or interupts.
-void FlightControl::setUpDigitalPins(){
-  // RGB LEDs
-  pinMode(R_LED, OUTPUT);
-  pinMode(G_LED, OUTPUT);
-  pinMode(B_LED, OUTPUT);
+void FlightControl::setUpDigitalPins()
+{
+    // RGB LEDs
+    pinMode(R_LED, OUTPUT);
+    pinMode(G_LED, OUTPUT);
+    pinMode(B_LED, OUTPUT);
 
-  // Turn on LEDs located on the arms
-  pinMode(LIGHT_1, OUTPUT); digitalWrite(LIGHT_1, HIGH);
-  pinMode(LIGHT_2, OUTPUT); digitalWrite(LIGHT_2, HIGH);
-  pinMode(LIGHT_3, OUTPUT); digitalWrite(LIGHT_3, HIGH);
-  pinMode(LIGHT_4, OUTPUT); digitalWrite(LIGHT_4, HIGH);
+    // Turn on LEDs located on the arms
+    pinMode(LIGHT_1, OUTPUT);
+    digitalWrite(LIGHT_1, HIGH);
+    pinMode(LIGHT_2, OUTPUT);
+    digitalWrite(LIGHT_2, HIGH);
+    pinMode(LIGHT_3, OUTPUT);
+    digitalWrite(LIGHT_3, HIGH);
+    pinMode(LIGHT_4, OUTPUT);
+    digitalWrite(LIGHT_4, HIGH);
 
-  // Receiver pin interupts
-  PCICR |= (1 << PCIE0);
-  PCMSK0 |= (1 << PCINT1);
-  PCMSK0 |= (1 << PCINT2);
-  PCMSK0 |= (1 << PCINT3);
-  PCMSK0 |= (1 << PCINT4);
-  PCMSK0 |= (1 << PCINT5);
+    // Receiver pin interupts
+    PCICR |= (1 << PCIE0);
+    PCMSK0 |= (1 << PCINT1);
+    PCMSK0 |= (1 << PCINT2);
+    PCMSK0 |= (1 << PCINT3);
+    PCMSK0 |= (1 << PCINT4);
+    PCMSK0 |= (1 << PCINT5);
 
-  // Set status LED to white to start
-  statusLight('W');
+    // Set status LED to white to start
+    statusLight('W');
 }
 
 /// @brief Configure flight battery for voltage monitoring
@@ -32,76 +37,79 @@ void FlightControl::setUpDigitalPins(){
 /// @param fullCellVoltage Full voltage per cell of battery (LiPo: 4.2 V/cell)
 /// @param R1 Value of first resistor in voltage divider circuit use the same units as R2
 /// @param R2 Value of second resistor in voltage divider circuit use the same units as R1
-void FlightControl::configureBattery(float numCells, float nominalCellVoltage, float fullCellVoltage, float R1, float R2){
-  battery.numCells = numCells;
-  battery.nominalCellVoltage = nominalCellVoltage;
-  battery.fullCellVoltage = fullCellVoltage;
-  battery.R1 = R1;
-  battery.R2 = R2;
+void FlightControl::configureBattery(float numCells, float nominalCellVoltage, float fullCellVoltage, float R1, float R2)
+{
+    battery.numCells = numCells;
+    battery.nominalCellVoltage = nominalCellVoltage;
+    battery.fullCellVoltage = fullCellVoltage;
+    battery.R1 = R1;
+    battery.R2 = R2;
 }
 
 /// @brief Sets the RGB light different colors to indicate the current status
 /// @param color Color options: Red 'R', Green 'G', Blue 'B', Cyan 'C', Magenta 'M', Yellow 'Y', and White 'W'
-void FlightControl::statusLight(char color){
+void FlightControl::statusLight(char color)
+{
     // Color based on state input
-    switch (color){
-      case 'R':
-      case 'r':
+    switch (color)
+    {
+    case 'R':
+    case 'r':
         // Red
         digitalWrite(R_LED, 1);
         digitalWrite(G_LED, 0);
         digitalWrite(B_LED, 0);
         break;
 
-      case 'G':
-      case 'g':
+    case 'G':
+    case 'g':
         // Green
         digitalWrite(R_LED, 0);
         digitalWrite(G_LED, 1);
         digitalWrite(B_LED, 0);
         break;
 
-      case 'B':
-      case 'b':
+    case 'B':
+    case 'b':
         // Blue
         digitalWrite(R_LED, 0);
         digitalWrite(G_LED, 0);
         digitalWrite(B_LED, 1);
         break;
 
-      case 'C':
-      case 'c':
+    case 'C':
+    case 'c':
         // Cyan
         digitalWrite(R_LED, 0);
         digitalWrite(G_LED, 1);
         digitalWrite(B_LED, 1);
         break;
 
-      case 'M':
-      case 'm':
+    case 'M':
+    case 'm':
         // Magenta
         digitalWrite(R_LED, 1);
         digitalWrite(G_LED, 0);
         digitalWrite(B_LED, 1);
         break;
 
-      case 'Y':
-      case 'y':
+    case 'Y':
+    case 'y':
         // Yellow
         digitalWrite(R_LED, 1);
         digitalWrite(G_LED, 1);
         digitalWrite(B_LED, 0);
         break;
 
-      case 'W':
-      case 'w':
+    case 'W':
+    case 'w':
         // White
         digitalWrite(R_LED, 1);
         digitalWrite(G_LED, 1);
         digitalWrite(B_LED, 1);
         break;
 
-      default:
+    default:
         // Black (off)
         digitalWrite(R_LED, 0);
         digitalWrite(G_LED, 0);
@@ -111,126 +119,153 @@ void FlightControl::statusLight(char color){
 }
 
 /// @brief Read battery voltage and flash arm lights if voltage is getting low.
-void FlightControl::monitorBattery(){
-  // Read battery voltage after it is passed through voltage divider and op-amp
-  int analogRaw = analogRead(BATTERY);
-  float voltageRaw = analogRaw * (5.0 / 1023.0);
-  float batteryVoltage = voltageRaw * (battery.R1 + battery.R2) / battery.R2;
+void FlightControl::monitorBattery()
+{
+    // Read battery voltage after it is passed through voltage divider and op-amp
+    int analogRaw = analogRead(BATTERY);
+    float voltageRaw = analogRaw * (5.0 / 1023.0);
+    float batteryVoltage = voltageRaw * (battery.R1 + battery.R2) / battery.R2;
 
-  // Flash lights if battery is starting to get low
-  if (batteryVoltage < (battery.nominalCellVoltage * battery.numCells)){ 
-    flashLights();
-  } else {
-    digitalWrite(LIGHT_1, HIGH);
-    digitalWrite(LIGHT_2, HIGH);
-    digitalWrite(LIGHT_3, HIGH);
-    digitalWrite(LIGHT_4, HIGH);
-  }
+    // Flash lights if battery is starting to get low
+    if (batteryVoltage < (battery.nominalCellVoltage * battery.numCells))
+    {
+        flashLights();
+    }
+    else
+    {
+        digitalWrite(LIGHT_1, HIGH);
+        digitalWrite(LIGHT_2, HIGH);
+        digitalWrite(LIGHT_3, HIGH);
+        digitalWrite(LIGHT_4, HIGH);
+    }
 }
 
 /// @brief Flash lights to indicate a low battery at 1 second intervals
-void FlightControl::flashLights(){
-  if ((micros() - lightTimer) > 1000){ 
-    digitalWrite(LIGHT_1, HIGH);
-    digitalWrite(LIGHT_2, HIGH);
-    digitalWrite(LIGHT_3, HIGH);
-    digitalWrite(LIGHT_4, HIGH);
-    lightTimer = micros();
-  } else {
-    digitalWrite(LIGHT_1, LOW);
-    digitalWrite(LIGHT_2, LOW);
-    digitalWrite(LIGHT_3, LOW);
-    digitalWrite(LIGHT_4, LOW);
-  }
+void FlightControl::flashLights()
+{
+    if ((micros() - lightTimer) > 1000)
+    {
+        digitalWrite(LIGHT_1, HIGH);
+        digitalWrite(LIGHT_2, HIGH);
+        digitalWrite(LIGHT_3, HIGH);
+        digitalWrite(LIGHT_4, HIGH);
+        lightTimer = micros();
+    }
+    else
+    {
+        digitalWrite(LIGHT_1, LOW);
+        digitalWrite(LIGHT_2, LOW);
+        digitalWrite(LIGHT_3, LOW);
+        digitalWrite(LIGHT_4, LOW);
+    }
 }
 
 /// @brief Initiate timers and desired loop rates.
 /// @param loopRateHz Desired loop rate in Hertz.
-void FlightControl::startTimers(int loopRateHz){
-  lightTimer = micros();
-  loopTimer = micros();
-  currentTime = micros();
-  desiredLoopRateMicroSec = (1.0 / (float)loopRateHz) * 1e6;
+void FlightControl::startTimers(int loopRateHz)
+{
+    lightTimer = micros();
+    loopTimer = micros();
+    currentTime = micros();
+    desiredLoopRateMicroSec = (1.0 / (float)loopRateHz) * 1e6;
 }
 
 /// @brief Add a delay so that there is a consistent loop rate
-void FlightControl::stabilizeLoopRate(){
-  // Calculate required delay
-  long timeToDelay = desiredLoopRateMicroSec - (micros() - previousTime);
+void FlightControl::stabilizeLoopRate()
+{
+    // Calculate required delay
+    long timeToDelay = desiredLoopRateMicroSec - (micros() - previousTime);
 
-  // Execute the delay
-  if (timeToDelay > 0){
-    delayMicroseconds(timeToDelay);
-  }
+    // Execute the delay
+    if (timeToDelay > 0)
+    {
+        delayMicroseconds(timeToDelay);
+    }
 
-  // Save previous time
-  previousTime = micros();
+    // Save previous time
+    previousTime = micros();
 }
 
 /// @brief Used in interrupt service routine to read current receiver values sent by the transmitter
-void FlightControl::receiverInterrupt(){
-  // Get current time
-  currentTime = micros();
+void FlightControl::receiverInterrupt()
+{
+    // Get current time
+    currentTime = micros();
 
-  // Channel 1
-  if(PINB & B00000001){
-    if(lastChannel1 == 0){
-      lastChannel1 = 1;
-      timer1 = currentTime;
+    // Channel 1
+    if (PINB & B00000001)
+    {
+        if (lastChannel1 == 0)
+        {
+            lastChannel1 = 1;
+            timer1 = currentTime;
+        }
     }
-  }
-  else if(lastChannel1 == 1){
-    lastChannel1 = 0;
-    rawRX.CH1 = currentTime - timer1;
-  }
+    else if (lastChannel1 == 1)
+    {
+        lastChannel1 = 0;
+        rawRX.CH1 = currentTime - timer1;
+    }
 
-  // Channel 2
-  if(PINB & B00000010){
-    if(lastChannel2 == 0){
-      lastChannel2 = 1;
-      timer2 = currentTime;
+    // Channel 2
+    if (PINB & B00000010)
+    {
+        if (lastChannel2 == 0)
+        {
+            lastChannel2 = 1;
+            timer2 = currentTime;
+        }
     }
-  }
-  else if(lastChannel2 == 1){
-    lastChannel2 = 0;
-    rawRX.CH2 = currentTime - timer2;
-  }
+    else if (lastChannel2 == 1)
+    {
+        lastChannel2 = 0;
+        rawRX.CH2 = currentTime - timer2;
+    }
 
-  // Channel 3
-  if(PINB & B00000100){
-    if(lastChannel3 == 0){
-      lastChannel3 = 1;
-      timer3 = currentTime;
+    // Channel 3
+    if (PINB & B00000100)
+    {
+        if (lastChannel3 == 0)
+        {
+            lastChannel3 = 1;
+            timer3 = currentTime;
+        }
     }
-  }
-  else if(lastChannel3 == 1){
-    lastChannel3 = 0;
-    rawRX.CH3 = currentTime - timer3;
-  }
+    else if (lastChannel3 == 1)
+    {
+        lastChannel3 = 0;
+        rawRX.CH3 = currentTime - timer3;
+    }
 
-  // Channel 4
-  if(PINB & B00001000){
-    if(lastChannel4 == 0){
-      lastChannel4 = 1;
-      timer4 = currentTime;
+    // Channel 4
+    if (PINB & B00001000)
+    {
+        if (lastChannel4 == 0)
+        {
+            lastChannel4 = 1;
+            timer4 = currentTime;
+        }
     }
-  }
-  else if(lastChannel4 == 1){
-    lastChannel4 = 0;
-    rawRX.CH4 = currentTime - timer4;
-  }
+    else if (lastChannel4 == 1)
+    {
+        lastChannel4 = 0;
+        rawRX.CH4 = currentTime - timer4;
+    }
 
-  // Channel 5
-  if(PINB & B00010000){
-    if(lastChannel5 == 0){
-      lastChannel5 = 1;
-      timer5 = currentTime;
+    // Channel 5
+    if (PINB & B00010000)
+    {
+        if (lastChannel5 == 0)
+        {
+            lastChannel5 = 1;
+            timer5 = currentTime;
+        }
     }
-  }
-  else if(lastChannel5 == 1){
-    lastChannel5 = 0;
-    rawRX.CH5 = currentTime - timer5;
-  }
+    else if (lastChannel5 == 1)
+    {
+        lastChannel5 = 0;
+        rawRX.CH5 = currentTime - timer5;
+    }
 }
 
 /// @brief Save calibration values of the RC receiver to ensure inputs are scaled correctly.
@@ -239,44 +274,56 @@ void FlightControl::receiverInterrupt(){
 /// @param channelCal3 Low, high, center, and direction of RC channel 3 based off calibration
 /// @param channelCal4 Low, high, center, and direction of RC channel 4 based off calibration
 /// @param channelCal5 Low, high, center, and direction of RC channel 5 based off calibration
-void FlightControl::saveReceiverCalibration(channel_t channelCal1, channel_t channelCal2, channel_t channelCal3, channel_t channelCal4, channel_t channelCal5){
-  _channelCal1 = channelCal1;
-  _channelCal2 = channelCal2;
-  _channelCal3 = channelCal3;
-  _channelCal4 = channelCal4;
-  _channelCal5 = channelCal5;
+void FlightControl::saveReceiverCalibration(channel_t channelCal1, channel_t channelCal2, channel_t channelCal3, channel_t channelCal4, channel_t channelCal5)
+{
+    _channelCal1 = channelCal1;
+    _channelCal2 = channelCal2;
+    _channelCal3 = channelCal3;
+    _channelCal4 = channelCal4;
+    _channelCal5 = channelCal5;
 }
 
 /// @brief Process the receiver value based on calibration
-void FlightControl::receiver(){
-  RX.CH1 = processReceiverInterrupt(rawRX.CH1, _channelCal1);
-  RX.CH2 = processReceiverInterrupt(rawRX.CH2, _channelCal2);
-  RX.CH3 = processReceiverInterrupt(rawRX.CH3, _channelCal3);
-  RX.CH4 = processReceiverInterrupt(rawRX.CH4, _channelCal4);
-  RX.CH5 = processReceiverInterrupt(rawRX.CH5, _channelCal5);
+void FlightControl::receiver()
+{
+    RX.CH1 = processReceiverInterrupt(rawRX.CH1, _channelCal1);
+    RX.CH2 = processReceiverInterrupt(rawRX.CH2, _channelCal2);
+    RX.CH3 = processReceiverInterrupt(rawRX.CH3, _channelCal3);
+    RX.CH4 = processReceiverInterrupt(rawRX.CH4, _channelCal4);
+    RX.CH5 = processReceiverInterrupt(rawRX.CH5, _channelCal5);
 }
 
-/// @brief Process the raw interupt receiver value to a properly scaled receiver value based on a prior calibration. 
+/// @brief Process the raw interupt receiver value to a properly scaled receiver value based on a prior calibration.
 /// @param input Raw input value received by the interupt.
-/// @param calibration RC calibration value structure. 
+/// @param calibration RC calibration value structure.
 /// @return Processed receiver value.
-int FlightControl::processReceiverInterrupt(int input, channel_t calibration){
-  // Input value is on the low side
-  if(input < calibration.center){
-    if(input < calibration.low) input = calibration.low;
-    int diff = ((long)(calibration.center - input) * (long)500) / (calibration.center - calibration.low);
-    if(calibration.reverse == 1) return 1500 + diff;
-    else return 1500 - diff;
-  }
-  // Input value is on the high side
-  else if(input > calibration.center){
-    if(input > calibration.high) input = calibration.high;
-    int diff = ((long)(input - calibration.center) * (long)500) / (calibration.high - calibration.center);
-    if(calibration.reverse == 1) return 1500 - diff;
-    else return 1500 + diff;
-  }
-  // Input value is dead center
-  else return 1500;
+int FlightControl::processReceiverInterrupt(int input, channel_t calibration)
+{
+    // Input value is on the low side
+    if (input < calibration.center)
+    {
+        if (input < calibration.low)
+            input = calibration.low;
+        int diff = ((long)(calibration.center - input) * (long)500) / (calibration.center - calibration.low);
+        if (calibration.reverse == 1)
+            return 1500 + diff;
+        else
+            return 1500 - diff;
+    }
+    // Input value is on the high side
+    else if (input > calibration.center)
+    {
+        if (input > calibration.high)
+            input = calibration.high;
+        int diff = ((long)(input - calibration.center) * (long)500) / (calibration.high - calibration.center);
+        if (calibration.reverse == 1)
+            return 1500 - diff;
+        else
+            return 1500 + diff;
+    }
+    // Input value is dead center
+    else
+        return 1500;
 }
 
 /// @brief Arm the drone if throttle is low and switch is toggled
--- Firmware/lib/FlightControl/FlightControl.h
+++ Firmware/lib/FlightControl/FlightControl.h
@@ -3,76 +3,80 @@
 #define FLIGHTCONTROL_H
 
 // Pinout
-#define BATTERY   A0
+#define BATTERY A0
 
-#define LIGHT_1   A1
-#define LIGHT_2   A2
-#define LIGHT_3   A3
-#define LIGHT_4   A4
+#define LIGHT_1 A1
+#define LIGHT_2 A2
+#define LIGHT_3 A3
+#define LIGHT_4 A4
 
-#define ESC_1     4
-#define ESC_2     5
-#define ESC_3     6
-#define ESC_4     7
+#define ESC_1 4
+#define ESC_2 5
+#define ESC_3 6
+#define ESC_4 7
 
-#define R_LED     11
-#define G_LED     12
-#define B_LED     13
+#define R_LED 11
+#define G_LED 12
+#define B_LED 13
 
-#define AUX_A     A5
-#define AUX_D     10
+#define AUX_A A5
+#define AUX_D 10
 
 // Battery monitoring structure
-struct batt_t {
-  float numCells, nominalCellVoltage, fullCellVoltage, R1, R2;
+struct batt_t
+{
+    float numCells, nominalCellVoltage, fullCellVoltage, R1, R2;
 };
 
 // Receiver calibration
-struct channel_t {
-  int low, high, center, reverse;
+struct channel_t
+{
+    int low, high, center, reverse;
 };
 
 // Receiver structure
-struct receiver_t {
-  int CH1, CH2, CH3, CH4, CH5;
+struct receiver_t
+{
+    int CH1, CH2, CH3, CH4, CH5;
 };
 
 // Class
-class FlightControl{
-private:
-  // Functions
-  void flashLights();
+class FlightControl
+{
+    private:
+        // Functions
+        void flashLights();
 
-  // Time Sync
-  long lightTimer;
-  long loopTimer;
-  long desiredLoopRateMicroSec;
-  long previousTime;
+        // Time Sync
+        long lightTimer;
+        long loopTimer;
+        long desiredLoopRateMicroSec;
+        long previousTime;
 
-  // Receiver
-  int processReceiverInterrupt(int input, channel_t calibration);
-  unsigned long timer1, timer2, timer3, timer4, timer5, currentTime;
-  char lastChannel1, lastChannel2, lastChannel3, lastChannel4, lastChannel5;
-  channel_t _channelCal1, _channelCal2, _channelCal3, _channelCal4, _channelCal5;
+        // Receiver
+        int processReceiverInterrupt(int input, channel_t calibration);
+        unsigned long timer1, timer2, timer3, timer4, timer5, currentTime;
+        char lastChannel1, lastChannel2, lastChannel3, lastChannel4, lastChannel5;
+        channel_t _channelCal1, _channelCal2, _channelCal3, _channelCal4, _channelCal5;
 
-  // Battery Monitoring
-  batt_t battery;
+        // Battery Monitoring
+        batt_t battery;
 
-public:
-  FlightControl() = default;
-  static void statusLight(char color);
-  void setUpDigitalPins();
-  void stabilizeLoopRate();
-  void startTimers(int loopRateHz=200);
-  void monitorBattery();
-  void configureBattery(float numCells=3, float nominalCellVoltage=3.7, float fullCellVoltage=4.2, float R1=3.24, float R2=2.00);
-  void receiverInterrupt();
-  void receiver();
-  void saveReceiverCalibration(channel_t channelCal1, channel_t channelCal2, channel_t channelCal3, channel_t channelCal4, channel_t channelCal5);
+    public:
+        FlightControl() = default;
+        static void statusLight(char color);
+        void setUpDigitalPins();
+        void stabilizeLoopRate();
+        void startTimers(int loopRateHz = 200);
+        void monitorBattery();
+        void configureBattery(float numCells = 3, float nominalCellVoltage = 3.7, float fullCellVoltage = 4.2, float R1 = 3.24, float R2 = 2.00);
+        void receiverInterrupt();
+        void receiver();
+        void saveReceiverCalibration(channel_t channelCal1, channel_t channelCal2, channel_t channelCal3, channel_t channelCal4, channel_t channelCal5);
 
-  // Receiver 
-  receiver_t RX;
-  receiver_t rawRX;
+        // Receiver
+        receiver_t RX;
+        receiver_t rawRX;
 };
 
 #endif // FLIGHTCONTROL_H
