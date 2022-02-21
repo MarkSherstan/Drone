5613a65cd8b6cb259f02b923c7bb39b78030b6a2
Mark Sherstan <mshersta@ualberta.ca>
2022-02-20 21:19:21 -0700

    Formatting

--- Firmware/lib/PID/PID.h
+++ Firmware/lib/PID/PID.h
@@ -1,37 +1,40 @@
-// Include gaurd
+// Include guard
 #ifndef PID_H
 #define PID_H
 
-// Limits of the system 
-#define MAX_ROLL_DEG    30
-#define MIN_ROLL_DEG   -30
+// Libraries 
+#include <arduino.h>
 
-#define MAX_PITCH_DEG   30
-#define MIN_PITCH_DEG  -30
-
-#define MAX_YAW_DEG     30
-#define MIN_YAW_DEG    -30
+// Limits
+#define MAX_ROLL_DEG   30
+#define MIN_ROLL_DEG  -30
+#define MAX_PITCH_DEG  30
+#define MIN_PITCH_DEG -30
+#define MAX_YAW_DEG    30
+#define MIN_YAW_DEG   -30
 
 // PID gains data structure
-struct gains_t {
-  float P, I, D;
+struct Gains
+{
+    float P, I, D;
 };
 
 // Class
-class PID{
-private:
-  // Variables
-  gains_t _gains;
-  float integral, prevError;
-  long timer; 
+class PID
+{
+    private:
+        // Variables
+        Gains _gains;
+        float integral, prevError;
+        long timer;
 
-public:
-  // Config
-  PID(gains_t gains);
-  
-  // Functions
-  float update(float actual, float desired=0);
-  void reset();
+    public:
+        // Config
+        PID(Gains gains);
+
+        // Functions
+        float update(float actual, float desired = 0);
+        void reset();
 };
 
 #endif // PID_H
